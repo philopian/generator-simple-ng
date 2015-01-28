2369c17c60887ab457eafb11a5ab582c73412eb7
philopian <pdotwdot@gmail.com>
2015-01-28 12:34:09 -0800

    sub-generator newRoute: added copy/template files to destination functionality

--- route/index.js
+++ route/index.js
@@ -14,25 +14,51 @@ var SimpleNgGenerator = yeoman.generators.NamedBase.extend({
   getAppName: function(){
 
   	// define variables
-	var ngModuleName = this._.camelize(this.appname)+"App"; 	
-	var userDefinedNgModuleName = this.appname;
-	var destDirPath = this.env.cwd;
-	var userDefinedRouteName = this.name;
-	var ngRouteName = this._.camelize(this.name); 
-	var ngControllerName = _.capitalize(this._.camelize(this.appname)+"Ctrl");
-	var ngRouteUrlName = "/#/"+this._.camelize(this.name);
+    var destDirPath = this.env.cwd;
+    var userDefinedNgModuleName = this.appname;
+  	var ngModuleName = this._.camelize(userDefinedNgModuleName)+"App"; 	
+  	var userDefinedRouteName = this.name;
+  	var ngRouteName = this._.camelize(userDefinedRouteName); 
+  	var ngRouteUrlName = "/"+ngRouteName;
+    var ngControllerName = _.capitalize(ngRouteName+"Ctrl");
+  	
 
   	// define some placeholder variables
     var routePlaceholderValues = {
-        NG_APP_NAME: 		ngModuleName,
-        ROUTE_NAME: 		ngRouteName,
-        CONTROLLER_NAME: 	ngControllerName,		
-        ROUTE_URL_NAME: 	ngRouteUrlName,
+        NG_APP_NAME: ngModuleName,
+        ROUTE_NAME: ngRouteName,
+        CONTROLLER_NAME: ngControllerName,		
+        ROUTE_URL_NAME: ngRouteUrlName,
     }
     console.log(routePlaceholderValues);
 
 
-
+    // copy template to dest dir
+    var basePath = destDirPath+"/app/"+ngRouteName;
+    this.copy(
+        "newRoute/_newRoute.css",
+        basePath+"/"+ngRouteName+".css"
+    );
+    this.template(  
+        "newRoute/_newRoute.controller.js",    
+        basePath+"/"+ngRouteName+".controller.js" ,
+        routePlaceholderValues
+    );
+    this.template(  
+        "newRoute/_newRoute.controller.spec.js",    
+        basePath+"/"+ngRouteName+".controller.spec.js",
+        routePlaceholderValues
+    );
+    this.template(  
+        "newRoute/_newRoute.html",    
+        basePath+"/"+ngRouteName+".html",
+        routePlaceholderValues
+    );
+    this.template(  
+        "newRoute/_newRoute.js",    
+        basePath+"/"+ngRouteName+".js",
+        routePlaceholderValues
+    );
 
 
 

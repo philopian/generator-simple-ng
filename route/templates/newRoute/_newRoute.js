1b552d42b7f1dd49ad5c318062c433889301b68e
philopian <pdotwdot@gmail.com>
2015-01-28 12:16:23 -0800

    sub-generator: added newRoute template files

---
+++ route/templates/newRoute/_newRoute.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('<%= NG_APP_NAME %>')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('<%= ROUTE_NAME %>', {
+        url: '<%= ROUTE_URL_NAME %>',
+        templateUrl: 'app/<%= ROUTE_NAME %>/<%= ROUTE_NAME %>.html',
+        controller: '<%= CONTROLLER_NAME %>'
+      });
+  });

daea743670effbf5561281ab3f4f7e475c2f6762
philopian <pdotwdot@gmail.com>
2015-01-30 12:32:55 -0800

    gulpfile: updated the gulpfile to add/remove Bower/cliend-side tags from the index.html file

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -13,28 +13,27 @@ var notify          = require("gulp-notify");
 
 
 
+
 //--DEV TASKS-------------------------------------------------------------------------
 
-// Remove 3rd party bower dependencies (scripts/links)
-gulp.task('cleanBowerDepends', function() {
+//--Remove all Bower dependency tags in the index.html file(scripts/links)
+gulp.task('cleanBowerTags', function() {
 
-    // regex pattern
+    // regex 3rd party
     var regexBowerCss   = /<!-- bower:css -->([\s\S]*?)<!-- endinject -->/g;
     var regexBowerJs    = /<!-- bower:js -->([\s\S]*?)<!-- endinject -->/g;
 
-    // clean tags
+    // clean 3rd party tags
     var cleanBowerCss   = "<!-- bower:css -->\n\t<!-- endinject -->";
     var cleanBowerJs    = "<!-- bower:js -->\n<!-- endinject -->";
 
     return gulp.src(['./webClient/index.html'])
-		       .pipe(replace(regexBowerCss, cleanBowerCss))
-		       .pipe(replace(regexBowerJs, cleanBowerJs))
-		       .pipe(gulp.dest('./webClient/'))
+           .pipe(replace(regexBowerCss, cleanBowerCss))
+           .pipe(replace(regexBowerJs, cleanBowerJs))
+           .pipe(gulp.dest('./webClient/'))
 });
-
-
-//--Inject all the Bower Dependencies
-gulp.task('injectBower', function () {
+//--Inject: all Bower dependency tags in the index.html file(scripts/links)
+gulp.task('injectBowerTags', function () {
     return gulp.src('./webClient/index.html')
                .pipe(inject(
                     gulp.src(mainBowerFiles({}), {read: false}),  {name: 'bower'}
@@ -43,17 +42,53 @@ gulp.task('injectBower', function () {
                .pipe(gulp.dest('./webClient'))
 });
 
-//--Remove all 3rd party dependencies and reinject them
-gulp.task('bower', function(callback) {
-    runSequence('cleanBowerDepends',['injectBower'], function(){
 
-      var sendMessage = "finished cleaning and installing all your bower dependencies!";
+//--Remove client-side files tags in the index.html file (scripts/links)
+gulp.task('cleanClientTags', function() {
 
-      return gulp.src('./webClient/index.html')
-                 .pipe(notify(sendMessage));
-    
-  });
+    // regex client
+    var regexInjectCss  = /<!-- inject:css -->([\s\S]*?)<!-- endinject -->/g;
+    var regexInjectJs   = /<!-- inject:js -->([\s\S]*?)<!-- endinject -->/g;
+
+    // clean tags
+    var cleanInjectCss  = "<!-- inject:css -->\n\t<!-- endinject -->";
+    var cleanInjectJs   = "<!-- inject:js -->\n\<!-- endinject -->";
+
+    return gulp.src(['./webClient/index.html'])
+           .pipe(replace(regexInjectCss, cleanInjectCss))
+           .pipe(replace(regexInjectJs, cleanInjectJs))
+           .pipe(gulp.dest('./webClient/'))
 });
+//--Inject client-side files tags in the index.html file (scripts/links)
+gulp.task('injectClientTags', function () {
+    var filterDevContent = [
+        './webClient/app/*.js',
+        './webClient/app/*.css',
+
+        './webClient/app/**/*.js',
+        '!./webClient/app/**/*.spec.js',
+
+        './webClient/app/ui-components/**/*.js',
+        '!./webClient/app/ui-components/**/*.spec.js',
+    ];
+
+    return gulp.src('./webClient/index.html')
+               .pipe(inject(
+                  gulp.src(filterDevContent, {read: false})
+               ))
+               .pipe(gulp.dest('./webClient'))
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -62,6 +97,27 @@ gulp.task('bower', function(callback) {
 
 //--BUILD TASKES----------------------------------------------------------------------
 
+//--Cleanup all the script/style tags in the index.html file
+gulp.task('cleanTags', function(callback) {
+    runSequence(['cleanClientTags','injectClientTags'],['cleanBowerTags','injectBowerTags'], function(){
+
+      var sendMessage = "Finished cleaning/re-injecting client-side and Bower tags into the index.html";
+
+      return gulp.src('./webClient/index.html')
+                 .pipe(notify(sendMessage));
+  });
+});
+
+//--Remove all the script/style tags in the index.html file
+gulp.task('xx', function(callback) {
+    runSequence('cleanClientTags','cleanBowerTags', function(){
+
+      var sendMessage = "You shouldn't have anymore Bower/client-side scripts/css tags in your index.html file!";
+
+      return gulp.src('./webClient/index.html')
+                 .pipe(notify(sendMessage));
+  });
+});
 
 
 

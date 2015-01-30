84803df48b9aea0d4ae51f449307a7a9cd00813c
philopian <pdotwdot@gmail.com>
2015-01-30 11:23:55 -0800

    gulpfile: added task called "bower" that remove all the bower dependencies (everything inside the <!-- bower:css --><!-- endinject --> && <!-- bower:js --><!-- endinject -->) and add everything in the bower.json file

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp')
 
 //--GULP PLUGINS----------------------------------------------------------------------
-
+var mainBowerFiles  = require('main-bower-files')
+var inject          = require("gulp-inject");
+var replace         = require('gulp-replace');
+var chalk           = require('chalk');
+var runSequence     = require('run-sequence');
+var notify          = require("gulp-notify");
 
 
 
@@ -10,9 +15,45 @@ var gulp = require('gulp')
 
 //--DEV TASKS-------------------------------------------------------------------------
 
+// Remove 3rd party bower dependencies (scripts/links)
+gulp.task('cleanBowerDepends', function() {
+
+    // regex pattern
+    var regexBowerCss   = /<!-- bower:css -->([\s\S]*?)<!-- endinject -->/g;
+    var regexBowerJs    = /<!-- bower:js -->([\s\S]*?)<!-- endinject -->/g;
+
+    // clean tags
+    var cleanBowerCss   = "<!-- bower:css -->\n\t<!-- endinject -->";
+    var cleanBowerJs    = "<!-- bower:js -->\n<!-- endinject -->";
+
+    return gulp.src(['./webClient/index.html'])
+		       .pipe(replace(regexBowerCss, cleanBowerCss))
+		       .pipe(replace(regexBowerJs, cleanBowerJs))
+		       .pipe(gulp.dest('./webClient/'))
+});
 
 
+//--Inject all the Bower Dependencies
+gulp.task('injectBower', function () {
+    return gulp.src('./webClient/index.html')
+               .pipe(inject(
+                    gulp.src(mainBowerFiles({}), {read: false}),  {name: 'bower'}
+               ))
+               .pipe(replace('/webClient/', ''))
+               .pipe(gulp.dest('./webClient'))
+});
 
+//--Remove all 3rd party dependencies and reinject them
+gulp.task('bower', function(callback) {
+    runSequence('cleanBowerDepends',['injectBower'], function(){
+
+      var sendMessage = "finished cleaning and installing all your bower dependencies!";
+
+      return gulp.src('./webClient/index.html')
+                 .pipe(notify(sendMessage));
+    
+  });
+});
 
 
 
@@ -31,9 +72,7 @@ var gulp = require('gulp')
 
 
 
-
-
-
+//--DEV-SERVER------------------------------------------------------------------------
 
 
 

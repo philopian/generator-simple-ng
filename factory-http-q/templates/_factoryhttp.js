db2cf9df01dbb9d6eef3c0ef75a2d7d28b83f690
philopian <pdotwdot@gmail.com>
2015-03-11 14:34:18 -0700

    added a new factory template: factory-http-q using promises

---
+++ factory-http-q/templates/_factoryhttp.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('<%= NG_APP_NAME %>')
+.factory('<%= FACTORY_NAME %>', function ($http,$q,BASE_URL) {
+
+  var baseUrl = BASE_URL.url+":"+BASE_URL.port;
+
+  return {
+      fetchData: function () {
+        return $http({ 
+                   method:'GET',
+                   url:baseUrl+'/api/test',
+                   headers:{'my-custom-header':"0.0.1"}
+               })
+               .then(function(response){
+                 // console.log(response);
+                 return response.data;
+               })
+               .catch(function(response){
+                 return $q.reject('Error. HTTP status ('+response.status+')');
+               });
+    },
+    postData: function (postDataObject) {
+      return $http({ 
+                 method:'POST',
+                 url:baseUrl+'/api/test',
+                 data:postDataObject
+             })
+             .then(function(response){
+               console.log(response);
+               return response.data;
+             })
+             .catch(function(response){
+               return $q.reject('Error. HTTP status ('+response.status+')');
+             });
+    }
+  };
+});

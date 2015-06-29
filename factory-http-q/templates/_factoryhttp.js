7eb2a7af613a5085d06bd1622fd704f6af11c89b
philopian <pdotwdot@gmail.com>
2015-06-28 19:07:31 -0700

    back to the old way

--- factory-http-q/templates/_factoryhttp.js
+++ factory-http-q/templates/_factoryhttp.js
@@ -1,88 +1,38 @@
 'use strict';
 
 angular.module('<%= NG_APP_NAME %>')
-.factory('<%= FACTORY_NAME %>', function($http, $q, BASE_URL){
-    var baseUrl = BASE_URL.url+":"+BASE_URL.port;
-    return {
-        fetchData: function(){
-            var deferred = $q.defer();
-            $http({ 
+.factory('<%= FACTORY_NAME %>', function ($http,$q,BASE_URL) {
+
+  var baseUrl = BASE_URL.url+":"+BASE_URL.port;
+
+  return {
+      fetchData: function () {
+        return $http({ 
                    method:'GET',
                    url:baseUrl+'/api/test',
                    headers:{'my-custom-header':"0.0.1"}
                })
-              .success(function(data){
-                  deferred.resolve(data);
-              })
-              .error(function(data){
-                deferred.resolve(data);
-              });
-            return deferred.promise;
-        },
-        postData: function(postDataObject){
-            var deferred = $q.defer();
-            $http({ 
+               .then(function(response){
+                 // console.log(response);
+                 return response.data;
+               })
+               .catch(function(response){
+                 return $q.reject('Error. HTTP status ('+response.status+')');
+               });
+    },
+    postData: function (postDataObject) {
+      return $http({ 
                  method:'POST',
                  url:baseUrl+'/api/test',
                  data:postDataObject
-              })
-              .success(function(data){
-                  deferred.resolve(data);
-              .error(function(data){
-                deferred.resolve(data);
-              });
-            return deferred.promise;
-        }
-
-    };
-})
-
-
-
-
-
-
-
-// .factory('<%= FACTORY_NAME %>', function ($http,$q,BASE_URL) {
-
-//   var baseUrl = BASE_URL.url+":"+BASE_URL.port;
-
-//   return {
-//       fetchData: function () {
-//         return $http({ 
-//                    method:'GET',
-//                    url:baseUrl+'/api/test',
-//                    headers:{'my-custom-header':"0.0.1"}
-//                })
-//                .then(function(response){
-//                  // console.log(response);
-//                  return response.data;
-//                })
-//                .catch(function(response){
-//                  return $q.reject('Error. HTTP status ('+response.status+')');
-//                });
-//     },
-
-
-
-
-
-
-
-//     postData: function (postDataObject) {
-//       return $http({ 
-//                  method:'POST',
-//                  url:baseUrl+'/api/test',
-//                  data:postDataObject
-//              })
-//              .then(function(response){
-//                console.log(response);
-//                return response.data;
-//              })
-//              .catch(function(response){
-//                return $q.reject('Error. HTTP status ('+response.status+')');
-//              });
-//     }
-//   };
-// })
-;
+             })
+             .then(function(response){
+               console.log(response);
+               return response.data;
+             })
+             .catch(function(response){
+               return $q.reject('Error. HTTP status ('+response.status+')');
+             });
+    }
+  };
+});
